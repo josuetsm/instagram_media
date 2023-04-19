823946088999b4b4b0ad2d130dfde000dc5ab971
josuetsm <josuetapia.sm@gmail.com>
2023-04-19 01:31:00 -0400

    Fix body

--- download_posts_v2.py
+++ download_posts_v2.py
@@ -28,7 +28,7 @@ def get_data_requests(username, data_type):
         return sum([post_data['items'] for post_data in posts_data], [])
 
 # Set the target username
-username = 'teletrece'
+username = '24horascl'
 
 # Create directories to store the scraped data
 os.makedirs(f'data/{username}/posts', exist_ok=True)
@@ -66,16 +66,13 @@ max_scroll_attempts = 10
 
 # Record the start time
 start_time = time.time()
-
 # Start a loop to scrape the posts
 while True:
     # Get the page source and parse it with PyQuery
     page_source = driver.page_source
     parsed_page = PyQuery(page_source)
-
     # Get the codes of the posts on the current page
     current_post_codes = [PyQuery(node).attr('href').split('/')[2] for node in parsed_page('div._aabd > a.x1i10hfl')]
-
     # If the last code on the current page is the same as the last code on the previous page, we've reached the end of the posts
     if current_post_codes[-1] == previous_post_codes[-1]:
         scroll_attempts += 1
@@ -86,34 +83,38 @@ while True:
             break
     else:
         scroll_attempts = 0
-
-    # Retrieve the post data for the current page
-    posts_data = get_data_requests(username, 'posts_data')
+    for attempts in range(10):
+    	try:
+	    # Retrieve the post data for the current page
+	    posts_data = get_data_requests(username, 'posts_data')
+	except:
+	    print('Retry get data requests')
+	    time.sleep(random.uniform(3, 4))
+	else:
+	    print('else')
+	    break
     posts_data = [item for item in posts_data if item['code'] not in codes]
     codes = codes.union([item['code'] for item in posts_data])
-
     # Write the page source and codes to disk
     with open(f"data/{username}/posts/data_{str(page_index).zfill(4)}.json", 'w', encoding="utf-8") as f:
         json.dump(posts_data, f)
     with open(f"data/{username}/codes.txt", "a", encoding="utf-8") as f:
         for code in current_post_codes:
             f.write(code + "\n")
-
     # Update the previous post codes variable and scroll down to load more posts
     previous_post_codes = current_post_codes.copy()
-
     # Delete requests
     del driver.requests
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
+    # Delete requests
+    del driver.requests
     # Add a random sleep to avoid bot detection
-    time.sleep(random.uniform(2, 3))
-
+    time.sleep(random.uniform(3, 4))
     # Print the current page index and the time elapsed since the loop started
     page_index += 1
-
     # Iterate over the posts and stop when a post before October 1, 2018 is found
-    if sum([item['device_timestamp'] < 1538352000000 for item in posts_data]) > 0:
+    if sum([item['taken_at'] < 1538352000 for item in posts_data]) > 0:
         break
-
     print(f"Scraped {page_index} pages in {time.time() - start_time:.2f} seconds")
+
+
